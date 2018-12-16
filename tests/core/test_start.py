8f0b6d0066771b6012338dba75c504ddf6b20c8e
rezolvent <rezx10m@gmail.com>
2018-12-16 23:25:32 +0300

    update test_help

--- tests/core/test_start.py
+++ tests/core/test_start.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from ...core.start import main
 
 
 class TestStart:
 
-    def test_help(self):
-        main('test')
+    def test_command_without_params(self):
+        with pytest.raises(ValueError) as exc_info:
+            main('test')
+
+        assert str(exc_info.value) == 'Не задана папка источник', \
+            'Should raise exception with correct message'
