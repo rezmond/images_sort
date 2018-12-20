edc3f887f363401742b58a1a698f7f6abddf6f65
rezolvent <rezx10m@gmail.com>
2018-12-20 22:22:25 +0300

    partial implement new test for the "scan" method

--- tests/core/test_sorter.py
+++ tests/core/test_sorter.py
@@ -28,3 +28,17 @@ class TestSorter:
             Sorter('test_1', 'test_2')
         assert 'test_1' in str(exc_info.value), \
             'Should catch not existed folder'
+
+    @mock.patch('os.path.isfile', return_value=True)
+    @mock.patch('os.listdir', return_value=iter((
+        'image-1.jpg',
+        'image-2.jpeg',
+        'image-3.jpeg',
+        'image-4.png',
+        'image-5.m4a',
+        'image-6.3gp',
+    )))
+    @mock.patch('os.path.isdir', side_effect=(True, True))
+    def test_scan(self, isdir, listdir, isfile):
+        sorter = Sorter('test_1', 'test_2')
+        sorter.scan()
