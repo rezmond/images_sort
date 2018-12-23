fa4374c7cb3a34e6690abd8c5b07ce099456ece4
rezolvent <rezx10m@gmail.com>
2018-12-23 23:15:03 +0300

    change moking work with FS to work with real FS

--- tests/core/test_sorter.py
+++ tests/core/test_sorter.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from unittest import mock
-
 import pytest
 
 from ...core.sorter import Sorter
@@ -16,29 +14,17 @@ class TestSorter:
             'Should catch not passed source folder'
 
         with pytest.raises(ValueError) as exc_info:
-            Sorter('test_1', None)
+            Sorter('tests/data', None)
         assert 'destination' in str(exc_info.value), \
             'Should catch not passed destination folder'
 
-        with mock.patch('os.path.isdir') as mocked_is_dir:
-            mocked_is_dir.side_effect = (True, True)
-            Sorter('test_1', 'test_2')
+        assert Sorter('tests/data', 'out'), 'Should be silent'
 
         with pytest.raises(ValueError) as exc_info:
             Sorter('test_1', 'test_2')
         assert 'test_1' in str(exc_info.value), \
             'Should catch not existed folder'
 
-    @mock.patch('os.path.isfile', return_value=True)
-    @mock.patch('os.listdir', return_value=iter((
-        'image-1.jpg',
-        'image-2.jpeg',
-        'image-3.jpeg',
-        'image-4.png',
-        'image-5.m4a',
-        'image-6.3gp',
-    )))
-    @mock.patch('os.path.isdir', side_effect=(True, True))
-    def test_scan(self, isdir, listdir, isfile):
-        sorter = Sorter('test_1', 'test_2')
+    def test_scan(self):
+        sorter = Sorter('tests/data', 'tests/data')
         sorter.scan()
