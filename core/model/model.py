7ef8311d307ee733e05726175865d87ff8138f7d
rezolvent <rezx10m@gmail.com>
2019-01-12 22:33:29 +0300

    move setting up source and destination folders to separate methods

--- core/model/model.py
+++ core/model/model.py
@@ -8,9 +8,13 @@ class MoverModel:
 
     def __init__(self):
         self._mover = Mover()
+        self._src_folder = None
+        self._dst_folder = None
 
-    def move(self, src_folder: str, dst_folder: str) -> MoveResult:
-        move_result = self._mover.move(src_folder, dst_folder)
+    def move(self) -> MoveResult:
+        assert self._src_folder and self._dst_folder, \
+            'You should set src and dst folder pathes before'
+        move_result = self._mover.move(self._src_folder, self._dst_folder)
         return move_result
 
     @property
@@ -20,3 +24,9 @@ class MoverModel:
     @on_image_moved.setter
     def on_image_moved(self, value):
         pass
+
+    def set_dst_folder(self, value: str) -> None:
+        self._dst_folder = value
+
+    def set_src_folder(self, value: str) -> None:
+        self._src_folder = value
