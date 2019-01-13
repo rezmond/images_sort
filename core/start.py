30e5fc6647a5e47a60464807bef99bcf1b446f50
rezolvent <rezx10m@gmail.com>
2019-01-13 19:20:55 +0300

    update start.py for the new implementation of program

--- core/start.py
+++ core/start.py
@@ -1,34 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import getopt
-import sys
-
-from .model.scanner import Scanner
+from .model import MoverModel
+from .controllers import ConsoleViewController
 
 
-def main(argv):
-    MAIN_PROGRAMM = 'start.py'
-    source_folder = ''
-    dst_folder = ''
-
-    try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
-    except getopt.GetoptError:
-        print('{0} -i <sourcefolder> -o <dstfolder>'.format(MAIN_PROGRAMM))
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print('{0} -i <source/folder> -o <dst/folder>'.format(MAIN_PROGRAMM))
-            sys.exit()
-        elif opt in ("-i", "--ifolder"):
-            source_folder = arg
-        elif opt in ("-o", "--ofolder"):
-            dst_folder = arg
-
-    scanner = Scanner()
-    scanner.scan(source_folder)
-
+def main():
+    model = MoverModel()
+    controller = ConsoleViewController(model)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
