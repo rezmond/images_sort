1a5f8373b80969aa85b73d13d59f7753cfcbea56
rezolvent <rezx10m@gmail.com>
2019-01-13 19:20:06 +0300

    minimal view implementation

---
+++ core/views/base.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+from ..model import MoverModel
+from ..controllers.base import ControllerBase
+
+
+class ViewBase(metaclass=ABCMeta):
+
+    view_class = None
+
+    def __init__(
+        self,
+        controller: ControllerBase,
+        model: MoverModel,
+    ):
+        self._controller = controller
+        self._model = model
+
+    @abstractmethod
+    def show(self):
+        pass
--- core/views/console.py
+++ core/views/console.py
@@ -1,5 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import getopt
+import sys
 
-class ConsoleView:
-    pass
+from .base import ViewBase
+
+
+MAIN_PROGRAMM = 'sorter.py'
+
+
+class ConsoleView(ViewBase):
+
+    def show(self):
+        argv = sys.argv[1:]
+        try:
+            opts, args = getopt.getopt(argv, "hi:o:", ["ifolder=", "ofolder="])
+        except getopt.GetoptError:
+            print('{0} -i <source/folder> -o <dst/folder>'.format(MAIN_PROGRAMM))
+            sys.exit(2)
+
+        for opt, arg in opts:
+            if opt == '-h':
+                print('{0} -i <source/folder> -o <dst/folder>'.format(MAIN_PROGRAMM))
+                sys.exit()
+            elif opt in ("-i", "--ifolder"):
+                self._controller.set_src_folder(arg)
+            elif opt in ("-o", "--ofolder"):
+                self._controller.set_dst_folder(arg)
+
+        self._model.move()
