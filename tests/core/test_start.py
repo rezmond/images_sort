87fe391ebf8f066612282625608cbe02ab792515
rezolvent <rezx10m@gmail.com>
2019-01-16 22:58:13 +0300

    delete redundant param

--- tests/core/test_start.py
+++ tests/core/test_start.py
@@ -9,7 +9,7 @@ class TestStart:
 
     def test_command_with_not_correct_params(self):
         with pytest.raises(ValueError) as exc_info:
-            main('test')
+            main()
 
         assert str(exc_info.value) == 'The source folder was not passed', \
             'Should raise exception with correct message'
