48a4fb0660739f35ec0fe4e8567286360077c029
rezolvent <rezx10m@gmail.com>
2019-04-07 12:02:35 +0300

    gets rid of magic values of the "exit" method

--- core/views/console.py
+++ core/views/console.py
@@ -4,7 +4,7 @@ import getopt
 import sys
 
 from .base import ViewBase
-
+from ..constants import COMMAND_LINE_SYNTAX_ERROR, SUCCESSFUL_TERMINATION
 
 MAIN_PROGRAMM = 'sorter.py'
 
@@ -17,12 +17,12 @@ class ConsoleView(ViewBase):
             opts, args = getopt.getopt(argv, "hi:o:", ["ifolder=", "ofolder="])
         except getopt.GetoptError:
             print('{0} -i <source/folder> -o <dst/folder>'.format(MAIN_PROGRAMM))
-            sys.exit(2)
+            sys.exit(COMMAND_LINE_SYNTAX_ERROR)
 
         for opt, arg in opts:
             if opt == '-h':
                 print('{0} -i <source/folder> -o <dst/folder>'.format(MAIN_PROGRAMM))
-                sys.exit()
+                sys.exit(SUCCESSFUL_TERMINATION)
             elif opt in ("-i", "--ifolder"):
                 self._controller.set_src_folder(arg)
             elif opt in ("-o", "--ofolder"):
