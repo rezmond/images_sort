25de802a3f3ebbaf712f6df999300765aed46d2f
rezolvent <rezx10m@gmail.com>
2019-04-07 12:04:14 +0300

    minorfix

--- tests/core/views/test_console.py
+++ tests/core/views/test_console.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-import sys
 
-from unittest.mock import patch, call, Mock
+from unittest.mock import patch
 
-from ....core.controllers import ConsoleViewController
 from ....core.views.console import ConsoleView
 
 
 class TestConsole:
 
     def test_filled_params(self):
-        with patch('images_sort.core.model.model.MoverModel') as patched_model,\
-                patch('images_sort.core.controllers.ConsoleViewController') as patched_controller:
+        with \
+            patch('images_sort.core.model.model.MoverModel') as patched_model,\
+            patch('images_sort.core.controllers.ConsoleViewController') \
+                as patched_controller:
             view = ConsoleView(patched_controller(), patched_model())
 
-        argv_data = [None, '-i', 'src', '-o', 'dst']
-        with patch('sys.argv', argv_data):
+        with patch('sys.argv', [None, '-i', 'src', '-o', 'dst']):
             view.show()
