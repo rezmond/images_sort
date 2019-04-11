96548ce3233ae6b0757aeb5b40b4d2e871d046e1
rezolvent <rezx10m@gmail.com>
2019-04-11 22:47:40 +0300

    implements the "test_help_param" test method

--- tests/core/views/test_console.py
+++ tests/core/views/test_console.py
@@ -1,27 +1,56 @@
 # -*- coding: utf-8 -*-
 
+import io
+import contextlib
+
+import pytest
+from unittest import TestCase
 from unittest.mock import patch, call
 
 from ....core.views.console import ConsoleView
 
 
-class TestConsole:
+class TestConsole(TestCase):
 
-    def test_filled_params(self):
+    def setUp(self):
         with \
             patch('images_sort.core.model.model.MoverModel') as patched_model,\
             patch('images_sort.core.controllers.ConsoleViewController') \
                 as patched_controller:
-            view = ConsoleView(patched_controller(), patched_model())
+            self._patched_controller = patched_controller()
+            self._patched_model = patched_model()
+            self._view = ConsoleView(
+                self._patched_controller,
+                self._patched_model,
+            )
+
+    def test_filled_params(self):
 
         with patch('sys.argv', [None, '-i', '/src/folder', '-o', '/dst/folder']):
-            view.show()
+            self._view.show()
 
-        patched_controller.assert_has_calls([
+        self._patched_controller.assert_has_calls([
             call.set_src_folder('/src/folder'),
             call.set_dst_folder('/dst/folder'),
         ])
 
-        patched_model.assert_has_calls([
+        self._patched_model.assert_has_calls([
             call.move(),
         ])
+
+    def test_help_param(self):
+        file_ = io.StringIO()
+        with contextlib.redirect_stdout(file_),\
+                pytest.raises(SystemExit) as exc_info,\
+                patch('sys.argv', [None, '-h']):
+            self._view.show()
+
+        assert exc_info.value.code == 0, 'Incorrect exit status'
+
+        assert (not self._patched_controller.mock_calls) \
+            and (not self._patched_model.mock_calls), \
+            'Nothing should be called when the help instruction was calling'
+
+        printed_help = file_.getvalue()
+        for x in (' -i ', ' -o '):
+            assert x in printed_help, 'Incorrect printed the help message'
