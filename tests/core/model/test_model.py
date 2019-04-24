f97a2f823d53bf68cb48595cfb23314b7db20980
rezolvent <rezx10m@gmail.com>
2019-04-24 11:55:16 +0300

    implements the MoverModel.on_image_moved property coverage by test

--- tests/core/model/test_model.py
+++ tests/core/model/test_model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from ....core.model.model import MoverModel
 from ....core.model.mover import Mover
@@ -16,3 +16,11 @@ class TestModel:
             model.move()
 
         patched_move.assert_called_with('src', 'dst')
+
+    def test_on_image_move_prop(self):
+        model = MoverModel()
+        with patch.object(Mover, 'on_image_moved') as patched_prop:
+            patched_prop.__get__ = Mock(return_value='')
+            patched_prop.__get__.assert_not_called()
+            model.on_image_moved
+            patched_prop.__get__.assert_called_once()
