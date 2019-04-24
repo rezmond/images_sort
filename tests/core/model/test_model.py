3894e61c31b407239bb83f44f7c1087b5518486c
rezolvent <rezx10m@gmail.com>
2019-04-24 11:22:49 +0300

    minorfix

--- tests/core/model/test_model.py
+++ tests/core/model/test_model.py
@@ -8,11 +8,11 @@ from ....core.model.mover import Mover
 
 class TestModel:
 
-    @patch.object(Mover, 'move')
-    def test_move_call(self, patched_move):
+    def test_move_call(self):
         model = MoverModel()
         model.set_dst_folder('dst')
         model.set_src_folder('src')
-        model.move()
+        with patch.object(Mover, 'move') as patched_move:
+            model.move()
 
         patched_move.assert_called_with('src', 'dst')
