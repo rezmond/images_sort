5863611ceb202b03846b1006ac137f5d92ab4b5e
rezolvent <rezx10m@gmail.com>
2019-04-27 22:12:30 +0300

    adds the tests for the ConsoleViewController

---
+++ tests/core/controllers/test_console.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from unittest.mock import patch, Mock
+
+
+from ....core.model import MoverModel
+from ....core.controllers.console import ConsoleViewController
+from ....core.views.console import ConsoleView
+
+
+class TestConsole:
+
+    def test_set_params(self):
+        model = Mock(spec=MoverModel)
+        with patch.object(ConsoleView, 'show') as patched_controller_show:
+            controller = ConsoleViewController(model)
+
+        controller.set_src_folder('/test/src/path')
+        controller.set_dst_folder('/test/dst/path')
+
+        patched_controller_show.assert_called_once()
+        model.set_src_folder.assert_called_once_with('/test/src/path')
+        model.set_dst_folder.assert_called_once_with('/test/dst/path')
