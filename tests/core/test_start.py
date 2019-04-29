55aff7b8fb0d82b880f2f66ed64cb03fc19611ee
rezolvent <rezx10m@gmail.com>
2019-04-29 22:39:53 +0300

    adds the test for the "move" method of the console controller

--- tests/core/test_start.py
+++ tests/core/test_start.py
@@ -9,5 +9,8 @@ class TestStart:
     def test_command_with_incorrect_params(self):
         with patch(
             'images_sort.core.start.ConsoleViewController',
-        ):
+        ) as patched_controller:
             main()
+
+        instance = patched_controller.return_value
+        instance.move.assert_called_once()
