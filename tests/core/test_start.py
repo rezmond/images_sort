68b32e2389032f2e1334bc43ab1d2b5be9a96a45
rezolvent <rezx10m@gmail.com>
2019-04-29 22:39:25 +0300

    makes the start test more independent

--- tests/core/test_start.py
+++ tests/core/test_start.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import pytest
-
 from unittest.mock import patch
 from ...core.start import main
 
@@ -9,11 +7,7 @@ from ...core.start import main
 class TestStart:
 
     def test_command_with_incorrect_params(self):
-        argv_data = [None]
-        with pytest.raises(ValueError) as exc_info, \
-                patch('sys.argv', argv_data):
+        with patch(
+            'images_sort.core.start.ConsoleViewController',
+        ):
             main()
-
-        assert str(exc_info.value)\
-            .startswith('The source folder\'s path did not set'), \
-            'Should raise exception with correct message'
