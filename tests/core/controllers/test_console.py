2583cd37aeae3f8459b4186b5d46b7204960080b
rezolvent <rezx10m@gmail.com>
2019-05-11 08:33:15 +0300

    minorfix of test

--- tests/core/controllers/test_console.py
+++ tests/core/controllers/test_console.py
@@ -5,27 +5,25 @@ from unittest.mock import patch, Mock
 
 from ....core.model import MoverModel
 from ....core.controllers.console import ConsoleViewController
-from ....core.views.console import ConsoleView
 
 
+@patch.object(ConsoleViewController, 'view_class', return_value=Mock())
 class TestConsole:
 
-    def test_set_params(self):
+    def test_set_params(self, patched_controller_view):
         model = Mock(spec=MoverModel)
-        with patch.object(ConsoleView, 'show') as patched_controller_show:
-            controller = ConsoleViewController(model)
+        controller = ConsoleViewController(model)
 
         controller.set_src_folder('/test/src/path')
         controller.set_dst_folder('/test/dst/path')
 
-        patched_controller_show.assert_called_once()
+        patched_controller_view.return_value.show.assert_called_once()
         model.set_src_folder.assert_called_once_with('/test/src/path')
         model.set_dst_folder.assert_called_once_with('/test/dst/path')
 
-    def test_move(self):
+    def test_move(self, patched_controller_view):
         model = Mock(spec=MoverModel)
-        with patch.object(ConsoleView, 'show'):
-            controller = ConsoleViewController(model)
+        controller = ConsoleViewController(model)
 
         controller.move()
         model.move.assert_called_once()
