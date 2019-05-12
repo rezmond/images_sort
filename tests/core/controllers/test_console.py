7518fcd90d3f1983e38b33a32b3d0cc1fb2f8274
rezolvent <rezx10m@gmail.com>
2019-05-12 22:51:13 +0300

    moves common code to the "setUp" method

--- tests/core/controllers/test_console.py
+++ tests/core/controllers/test_console.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from unittest.mock import patch, Mock
-
+from unittest import TestCase
 
 from ....core.model import MoverModel
 from ....core.controllers.console import ConsoleViewController
 
 
-@patch.object(ConsoleViewController, 'view_class', return_value=Mock())
-class TestConsole:
+class TestConsole(TestCase):
 
-    def test_set_params(self, patched_controller_view):
-        model = Mock(spec=MoverModel)
-        controller = ConsoleViewController(model)
+    def setUp(self):
+        patcher = patch.object(
+            ConsoleViewController, 'view_class', return_value=Mock())
+        self._patched_controller_view = patcher.start()
+        self._model = Mock(spec=MoverModel)
+        self._controller = ConsoleViewController(self._model)
 
-        controller.set_src_folder('/test/src/path')
-        controller.set_dst_folder('/test/dst/path')
+    def test_set_params(self):
+        self._controller.set_src_folder('/test/src/path')
+        self._controller.set_dst_folder('/test/dst/path')
 
-        patched_controller_view.return_value.show.assert_called_once()
-        model.set_src_folder.assert_called_once_with('/test/src/path')
-        model.set_dst_folder.assert_called_once_with('/test/dst/path')
+        self._patched_controller_view.return_value.show.assert_called_once()
+        self._model.set_src_folder.assert_called_once_with('/test/src/path')
+        self._model.set_dst_folder.assert_called_once_with('/test/dst/path')
 
-    def test_move(self, patched_controller_view):
-        model = Mock(spec=MoverModel)
-        controller = ConsoleViewController(model)
-
-        controller.move()
-        model.move.assert_called_once()
+    def test_move(self):
+        self._controller.move()
+        self._model.move.assert_called_once()
