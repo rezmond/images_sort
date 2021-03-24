d999f94f34f561f51d4f3c8a6021152c1ceedd41
rezolvent <rezx10m@gmail.com>
2021-03-24 18:07:33 +0300

    Clear code

--- core/entities/mover/mover.py
+++ core/entities/mover/mover.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-from typing import Callable
+from functools import partial
+from typing import Callable, Dict, Tuple
 
 from typeguard import typechecked
 
@@ -30,40 +31,43 @@ class Mover(MoverBase):
         self._moved_image_event_listeners = observable_factory()
         self._move_finish_event_listeners = observable_factory()
 
-    def _cmp_files(self, dst_dir, file_dict):
+    @typechecked
+    def _cmp_files(
+        self,
+        dst_dir: str,
+        file_dict: Dict[str, str]
+    ) -> Tuple[bool, str]:
         """
         Check the destination folder for already existed files with the
         same names.
 
         If a file exists and it is identical, then the current method
-        will return fully path to target folder with the "already_exists"
-        result type.
+        will return fully path to target folder with the False value of
+        the first argument.
 
         If a file with that name exists but it is not identical, then the
         current method will rename the target file name added a number til
         the name will unique.
-
-        :return: (<result_type>, <fully/path/to/file>)
-        :rtype: typle(str, str)
         """
-        num = 1
         curr_file_name = os.path.split(file_dict['path'])[1]
         dst_file_path = os.path.join(dst_dir, curr_file_name)
 
         if not os.path.isfile(dst_file_path):
-            return 'moved', dst_file_path
+            return True, dst_file_path
 
         base_file_name, extension = os.path.splitext(curr_file_name)
+        compare = partial(self._fs_actions.compare, dst=dst_file_path)
 
+        num = 1
         while os.path.isfile(dst_file_path):
-            if self._fs_actions.compare(file_dict['path'], dst_file_path):
-                return 'already_exists', dst_file_path
+            if compare(file_dict['path']):
+                return False, dst_file_path
 
-            curr_file_name = '{}_{}{}'.format(
-                base_file_name, num, extension)
+            curr_file_name = f'{base_file_name}_{num}{extension}'
             dst_file_path = os.path.join(dst_dir, curr_file_name)
             num += 1
-        return 'moved', dst_file_path
+
+        return True, dst_file_path
 
     def _make_dir_if_not_exists(self, path):
         """Если целевой папки не было создано"""
@@ -106,18 +110,24 @@ class Mover(MoverBase):
     @typechecked
     def _move_by_cmp(self, m_value: BlocksType, dst_dir_path: str) -> None:
         for file_dict in m_value:
-            result_type, result_path = (
-                self._cmp_files(dst_dir_path, file_dict))
+            to_move, result_path = self._cmp_files(dst_dir_path, file_dict)
 
-            if result_type == 'moved':
-                self._fs_actions.move(file_dict['path'], result_path)
-                self._moved_image_event_listeners.update(
-                    (file_dict['path'], result_path))
-            elif result_type == 'already_exists':
-                self._fs_actions.delete(file_dict['path'])
+            if to_move:
+                self._physical_move(file_dict['path'], result_path)
+                continue
 
-            getattr(self._move_result, result_type)\
-                .append(file_dict['path'])
+            self._resolve_conflict(file_dict['path'])
+
+    @typechecked
+    def _physical_move(self, path: str, result_path: str) -> None:
+        self._fs_actions.move(path, result_path)
+        self._moved_image_event_listeners.update((path, result_path))
+        self._move_result.moved.append(path)
+
+    @typechecked
+    def _resolve_conflict(self, path: str) -> None:
+        self._fs_actions.delete(path)
+        self._move_result.already_exists.append(path)
 
     @MoverBase.on_image_moved.getter
     def on_image_moved(self):
