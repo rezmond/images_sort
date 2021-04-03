38ab19619d2f2db36964b1003a696dff767b3785
rezolvent <rezx10m@gmail.com>
2021-04-03 11:56:02 +0300

    Freed the "test_scan" test case from real io operations

--- tests/core/model/test_scanner.py
+++ tests/core/model/test_scanner.py
@@ -1,57 +1,121 @@
+import os
+import contextlib
+from datetime import date
 from unittest.mock import call, Mock
 
 import pytest
+import yaml
 
-from containers import Container
 from core.entities import DateExtractorBase, FolderExtractorBase
+from core.types import FileDescriptor
 from core.utils.base import Observable
 from utils import full_path
 from ...utils import assert_dict_equal
-from .fixtures import get_move_map
 
 PATH_TO_TEST_DATA = full_path('tests/data')
 
 
 @pytest.fixture
-def container():
-    ioc = Container()
-    yield ioc
+def expected_move_map():
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, './fixtures/move_map.yml')
+    with open(file_path, 'r') as file_:
+        move_map = yaml.full_load(file_)
+
+    for year_values in move_map.values():
+        for preiod_name, period_values in year_values.items():
+            year_values[preiod_name] = [
+                FileDescriptor(
+                    value['path'],
+                    value['name']
+                ) for value in period_values
+            ]
+
+    return move_map
 
 
-def test_scan(container):
-    scanner = container.scanner()
+def get_scanner(container, **kwargs):
+    mocks = {**{
+        'date_extractor': Mock(spec=DateExtractorBase),
+        'observable': Mock(spec=Observable),
+        'fs_manipulator': Mock(spec=FolderExtractorBase),
+        'validator': Mock(),
+    }, **kwargs}
+    with container.date_extractor.override(mocks['date_extractor']),\
+            container.observable.override(mocks['observable']),\
+            container.fs_manipulator.override(mocks['fs_manipulator']),\
+            container.folder_path_validator.override(mocks['validator']):
+        scanner = container.scanner()
+    return scanner
+
+
+def test_scan_no_path_error(container):
+    scanner = get_scanner(container)
 
     with pytest.raises(TypeError) as exc_info:
         scanner.scan(None)
+
     assert str(exc_info.value) == 'type of argument "src_folder_path" '\
         'must be str; got NoneType instead', \
         'Should catch not passed source folder'
 
-    with pytest.raises(ValueError) as exc_info:
-        scanner.scan('test_1')
-    assert 'absolute' in str(exc_info.value), \
-        'Should catch not absolute the source folder path'
+
+def test_scan_path_validation_error(container):
+    validator_mock = Mock(side_effect=ValueError('test message'))
+    scanner = get_scanner(container, validator=validator_mock)
 
     with pytest.raises(ValueError) as exc_info:
-        scanner.scan(full_path('test_1'))
-    assert 'test_1' in str(exc_info.value), \
-        'Should catch not existed folder'
+        scanner.scan('/test_1')
 
-    scanner.scan(PATH_TO_TEST_DATA)
-    move_map, no_data, not_images = scanner.get_data()
+    assert str(exc_info.value) == 'test message',\
+        'Should raise validation errors'
 
-    expected_move_map = get_move_map()
-    assert_dict_equal(move_map, expected_move_map,
+
+def test_scan(container, expected_move_map):
+    get_date_mock = {
+        '/test/path/1.jpg': date.fromisoformat('2017-01-15'),
+        '/test/path/2.jpg': date.fromisoformat('2017-03-14'),
+        '/test/path/3.jpg': date.fromisoformat('2017-06-20'),
+        '/test/path/5.jpg': date.fromisoformat('2017-12-02'),
+        '/test/path/4.jpg': date.fromisoformat('2017-12-30'),
+        '/test/path/folder-1/1-1.jpg': None,
+        '/test/path/video1.mp4': None,
+    }.__getitem__
+    date_extractor_mock = Mock(spec=DateExtractorBase, **{
+        'is_allowed_extension.return_value': True,
+        'get_date': get_date_mock,
+    })
+    fs_manipulator_mock = Mock(spec=FolderExtractorBase, **{
+        'folder_to_file_pathes.return_value': (
+            ('1.jpg', '/test/path/1.jpg'),
+            ('2.jpg', '/test/path/2.jpg'),
+            ('3.jpg', '/test/path/3.jpg'),
+            ('5.jpg', '/test/path/5.jpg'),
+            ('4.jpg', '/test/path/4.jpg'),
+            ('1-1.jpg', '/test/path/folder-1/1-1.jpg'),
+            ('video1.mp4', '/test/path/video1.mp4'),
+        ),
+    })
+    scanner = get_scanner(
+        container,
+        date_extractor=date_extractor_mock,
+        fs_manipulator=fs_manipulator_mock,
+    )
+
+    scanner.scan('')
+    scanned = scanner.get_data()
+
+    assert_dict_equal(scanned.move_map, expected_move_map,
                       'Should return correct move_map')
 
     expected_no_exif = [
-        full_path('tests/data/video1.mp4'),
-        full_path('tests/data/folder-1/1-1.jpg'),
+        '/test/path/video1.mp4',
+        '/test/path/folder-1/1-1.jpg',
     ]
-    assert no_data == expected_no_exif, 'Should return correct no_data'
+    assert scanned.no_data == expected_no_exif, 'Should return correct no_data'
 
     expected_not_media = []
-    assert not_images == expected_not_media, \
+    assert scanned.not_media == expected_not_media, \
         'Should return correct not_media'
 
 
