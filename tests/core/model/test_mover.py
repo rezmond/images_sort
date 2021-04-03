59b10f314cf5c6b00e850e8090edcfe78bf96de7
rezolvent <rezx10m@gmail.com>
2021-04-03 09:18:36 +0300

    Delete redundant code

--- tests/core/model/test_mover.py
+++ tests/core/model/test_mover.py
@@ -31,116 +31,116 @@ def check_subscription(container):
     observable_mock.__iadd__.assert_called_once_with(handler_mock)
 
 
-class TestMover:
-
-    def test_move_without_dst_param(self, container):
-        fs_manipulator_mock = Mock(spec=FsManipulatorBase)
-        with container.fs_manipulator.override(fs_manipulator_mock),\
-                container.comparator.override(Mock()),\
-                pytest.raises(ValueError) as exc_info:
-            mover = container.mover()
-            mover.move(ScanResult(None, None, None), None)
-
-        assert 'path did not set' in str(exc_info.value), \
-            'Should catch not set the src or the dst folder path'
-
-    def test_move_by_relative_path(self, container):
-        fs_manipulator_mock = Mock(spec=FsManipulatorBase)
-
-        with container.fs_manipulator.override(fs_manipulator_mock),\
+def test_move_without_dst_param(container):
+    fs_manipulator_mock = Mock(spec=FsManipulatorBase)
+    with container.fs_manipulator.override(fs_manipulator_mock),\
             container.comparator.override(Mock()),\
-                pytest.raises(ValueError) as exc_info:
-            mover = container.mover()
-            mover.move(ScanResult(None, None, None), 'test-1')
+            pytest.raises(ValueError) as exc_info:
+        mover = container.mover()
+        mover.move(ScanResult(None, None, None), None)
 
-        assert 'absolute' in str(exc_info.value), \
-            'Should catch not absolute the destination folder path'
+    assert 'path did not set' in str(exc_info.value), \
+        'Should catch not set the src or the dst folder path'
 
-    def test_move_by_absolute_path(self, container):
-        def comporator_mock(x, _):
-            return x.endswith('1.jpg')
 
-        on_item_moved_handler_mock = Mock()
-        fs_manipulator_mock = Mock(spec=FsManipulatorBase)
+def test_move_by_relative_path(container):
+    fs_manipulator_mock = Mock(spec=FsManipulatorBase)
 
-        with container.fs_manipulator.override(fs_manipulator_mock),\
-                container.comparator.override(comporator_mock):
-            mover = container.mover()
-            mover.on_image_moved += on_item_moved_handler_mock
+    with container.fs_manipulator.override(fs_manipulator_mock),\
+        container.comparator.override(Mock()),\
+            pytest.raises(ValueError) as exc_info:
+        mover = container.mover()
+        mover.move(ScanResult(None, None, None), 'test-1')
 
-            move_result = mover.move(
-                ScanResult({
-                    '2017': {
-                        'spring': [
-                            FileDescriptor(full_path('tests/data/2.jpg'), '')
-                        ],
-                        'summer': [
-                            FileDescriptor(full_path('tests/data/3.jpg'), '')
-                        ],
-                        'winter (end)': [
-                            FileDescriptor(full_path('tests/data/5.jpg'), ''),
-                            FileDescriptor(full_path('tests/data/4.jpg'), '')
-                        ],
-                        'winter (begin)': [
-                            FileDescriptor(full_path('tests/data/1.jpg'), '')
-                        ],
-                        'summer': [
-                            FileDescriptor(full_path('tests/data/3.jpg'), '')
-                        ],
-                    }
-                }, [], []), full_path('tests/out'))
+    assert 'absolute' in str(exc_info.value), \
+        'Should catch not absolute the destination folder path'
 
-        calls = [
-            call(
-                full_path('tests/data/2.jpg'),
-                full_path('tests/out/2017/spring/2_1.jpg')
-            ),
-            call(
-                full_path('tests/data/5.jpg'),
-                full_path('tests/out/2017/winter (end)/5.jpg')
-            ),
-            call(
-                full_path('tests/data/3.jpg'),
-                full_path('tests/out/2017/summer/3.jpg')
-            ),
-            call(
-                full_path('tests/data/4.jpg'),
-                full_path('tests/out/2017/winter (end)/4.jpg')
-            ),
-        ]
 
-        copy_mock = fs_manipulator_mock.copy
-        copy_mock.assert_has_calls(calls, any_order=True)
+def test_move_by_absolute_path(container):
+    def comporator_mock(x, _):
+        return x.endswith('1.jpg')
 
-        calls = [
-            call((
-                full_path('tests/data/2.jpg'),
-                full_path('tests/out/2017/spring/2_1.jpg')
-            )),
-            call((
-                full_path('tests/data/3.jpg'),
-                full_path('tests/out/2017/summer/3.jpg')
-            )),
-            call((
-                full_path('tests/data/5.jpg'),
-                full_path('tests/out/2017/winter (end)/5.jpg')
-            )),
-            call((
-                full_path('tests/data/4.jpg'),
-                full_path('tests/out/2017/winter (end)/4.jpg')
-            )),
-        ]
-        on_item_moved_handler_mock\
-            .assert_has_calls(calls, any_order=True)
+    on_item_moved_handler_mock = Mock()
+    fs_manipulator_mock = Mock(spec=FsManipulatorBase)
 
-        assert move_result == ([
+    with container.fs_manipulator.override(fs_manipulator_mock),\
+            container.comparator.override(comporator_mock):
+        mover = container.mover()
+        mover.on_image_moved += on_item_moved_handler_mock
+
+        move_result = mover.move(
+            ScanResult({
+                '2017': {
+                    'spring': [
+                        FileDescriptor(full_path('tests/data/2.jpg'), '')
+                    ],
+                    'summer': [
+                        FileDescriptor(full_path('tests/data/3.jpg'), '')
+                    ],
+                    'winter (end)': [
+                        FileDescriptor(full_path('tests/data/5.jpg'), ''),
+                        FileDescriptor(full_path('tests/data/4.jpg'), '')
+                    ],
+                    'winter (begin)': [
+                        FileDescriptor(full_path('tests/data/1.jpg'), '')
+                    ],
+                    'summer': [
+                        FileDescriptor(full_path('tests/data/3.jpg'), '')
+                    ],
+                }
+            }, [], []), full_path('tests/out'))
+
+    calls = [
+        call(
             full_path('tests/data/2.jpg'),
-            full_path('tests/data/3.jpg'),
+            full_path('tests/out/2017/spring/2_1.jpg')
+        ),
+        call(
             full_path('tests/data/5.jpg'),
+            full_path('tests/out/2017/winter (end)/5.jpg')
+        ),
+        call(
+            full_path('tests/data/3.jpg'),
+            full_path('tests/out/2017/summer/3.jpg')
+        ),
+        call(
             full_path('tests/data/4.jpg'),
-        ], [
-            full_path('tests/data/1.jpg'),
-        ], [], []), 'Should return correct MoveResult'
+            full_path('tests/out/2017/winter (end)/4.jpg')
+        ),
+    ]
+
+    copy_mock = fs_manipulator_mock.copy
+    copy_mock.assert_has_calls(calls, any_order=True)
+
+    calls = [
+        call((
+            full_path('tests/data/2.jpg'),
+            full_path('tests/out/2017/spring/2_1.jpg')
+        )),
+        call((
+            full_path('tests/data/3.jpg'),
+            full_path('tests/out/2017/summer/3.jpg')
+        )),
+        call((
+            full_path('tests/data/5.jpg'),
+            full_path('tests/out/2017/winter (end)/5.jpg')
+        )),
+        call((
+            full_path('tests/data/4.jpg'),
+            full_path('tests/out/2017/winter (end)/4.jpg')
+        )),
+    ]
+    on_item_moved_handler_mock\
+        .assert_has_calls(calls, any_order=True)
+
+    assert move_result == ([
+        full_path('tests/data/2.jpg'),
+        full_path('tests/data/3.jpg'),
+        full_path('tests/data/5.jpg'),
+        full_path('tests/data/4.jpg'),
+    ], [
+        full_path('tests/data/1.jpg'),
+    ], [], []), 'Should return correct MoveResult'
 
 
 def test_on_move_finished_subscribe(container):
--- tests/core/model/test_scanner.py
+++ tests/core/model/test_scanner.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import pytest
 from unittest.mock import call, Mock
 
@@ -17,58 +15,57 @@ def container():
     yield ioc
 
 
-class TestScanner:
+def test_scan(container):
+    scanner = container.scanner()
 
-    def test_scan(self, container):
-        scanner = container.scanner()
+    with pytest.raises(TypeError) as exc_info:
+        scanner.scan(None)
+    assert str(exc_info.value) == 'type of argument "src_folder_path" '\
+        'must be str; got NoneType instead', \
+        'Should catch not passed source folder'
 
-        with pytest.raises(TypeError) as exc_info:
-            scanner.scan(None)
-        assert str(exc_info.value) == 'type of argument "src_folder_path" '\
-            'must be str; got NoneType instead', \
-            'Should catch not passed source folder'
+    with pytest.raises(ValueError) as exc_info:
+        scanner.scan('test_1')
+    assert 'absolute' in str(exc_info.value), \
+        'Should catch not absolute the source folder path'
 
-        with pytest.raises(ValueError) as exc_info:
-            scanner.scan('test_1')
-        assert 'absolute' in str(exc_info.value), \
-            'Should catch not absolute the source folder path'
+    with pytest.raises(ValueError) as exc_info:
+        scanner.scan(full_path('test_1'))
+    assert 'test_1' in str(exc_info.value), \
+        'Should catch not existed folder'
 
-        with pytest.raises(ValueError) as exc_info:
-            scanner.scan(full_path('test_1'))
-        assert 'test_1' in str(exc_info.value), \
-            'Should catch not existed folder'
+    scanner.scan(PATH_TO_TEST_DATA)
+    move_map, no_data, not_images = scanner.get_data()
 
-        scanner.scan(PATH_TO_TEST_DATA)
-        move_map, no_data, not_images = scanner.get_data()
+    expected_move_map = get_move_map()
+    assert_dict_equal(move_map, expected_move_map,
+                      'Should return correct move_map')
 
-        expected_move_map = get_move_map()
-        assert_dict_equal(move_map, expected_move_map,
-                          'Should return correct move_map')
+    expected_no_exif = [
+        full_path('tests/data/video1.mp4'),
+        full_path('tests/data/folder-1/1-1.jpg'),
+    ]
+    assert no_data == expected_no_exif, 'Should return correct no_data'
 
-        expected_no_exif = [
-            full_path('tests/data/video1.mp4'),
-            full_path('tests/data/folder-1/1-1.jpg'),
-        ]
-        assert no_data == expected_no_exif, 'Should return correct no_data'
+    expected_not_media = []
+    assert not_images == expected_not_media, \
+        'Should return correct not_media'
 
-        expected_not_media = []
-        assert not_images == expected_not_media, \
-            'Should return correct not_media'
 
-    def test_found_items(self, container):
-        scanner = container.scanner()
-        handler_mock = Mock()
-        scanner.on_file_found += handler_mock
-        scanner.scan(PATH_TO_TEST_DATA)
+def test_found_items(container):
+    scanner = container.scanner()
+    handler_mock = Mock()
+    scanner.on_file_found += handler_mock
+    scanner.scan(PATH_TO_TEST_DATA)
 
-        expected_calls = [
-            call(full_path('tests/data/2.jpg')),
-            call(full_path('tests/data/1.jpg')),
-            call(full_path('tests/data/3.jpg')),
-            call(full_path('tests/data/video1.mp4')),
-            call(full_path('tests/data/5.jpg')),
-            call(full_path('tests/data/folder-1/1-1.jpg')),
-            call(full_path('tests/data/4.jpg')),
-        ]
+    expected_calls = [
+        call(full_path('tests/data/2.jpg')),
+        call(full_path('tests/data/1.jpg')),
+        call(full_path('tests/data/3.jpg')),
+        call(full_path('tests/data/video1.mp4')),
+        call(full_path('tests/data/5.jpg')),
+        call(full_path('tests/data/folder-1/1-1.jpg')),
+        call(full_path('tests/data/4.jpg')),
+    ]
 
-        handler_mock.assert_has_calls(expected_calls, any_order=True)
+    handler_mock.assert_has_calls(expected_calls, any_order=True)
