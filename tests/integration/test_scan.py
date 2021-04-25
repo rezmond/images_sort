9f39d885145b0c68c71fd57fe5b7bde20466ce55
rezolvent <rezx10m@gmail.com>
2021-04-25 09:45:00 +0300

    Implement the first part of integration tests

---
+++ tests/integration/test_scan.py
@@ -0,0 +1,55 @@
+import io
+import contextlib
+from datetime import date
+from unittest.mock import patch, Mock
+
+from core.entities import (
+    FolderExtractorBase,
+    FsManipulatorBase,
+)
+from core.system_interfaces import FolderCheckerBase
+
+
+class FsManipulatorCompilation(
+        FolderCheckerBase, FsManipulatorBase, FolderExtractorBase):
+    pass
+
+
+base_path_map = (
+    ('/src/path/1.jpg', date.fromisoformat('2017-01-15')),
+    ('/src/path/2.jpg', date.fromisoformat('2017-03-14')),
+    ('/src/path/3.jpg', date.fromisoformat('2017-06-20')),
+    ('/src/path/4.jpg', date.fromisoformat('2017-12-30')),
+)
+
+base_pathes = [path for (path, _) in base_path_map]
+
+
+def get_fs_manipulator_mock():
+    return Mock(spec=FsManipulatorCompilation, **{
+        'folder_to_file_pathes.return_value': base_pathes,
+    })
+
+
+def call_view(container, argv_args):
+    fs_manipulator_mock = get_fs_manipulator_mock()
+    with patch('sys.argv', argv_args), \
+            container.fs_manipulator.override(fs_manipulator_mock):
+        view_class = container.view_class()
+        controller = container.controller()
+        model = container.model()
+        view = view_class(controller, model)
+        view.show()
+
+
+def test_scan_log(container):
+    caught_io = io.StringIO()
+    with contextlib.redirect_stdout(caught_io):
+        call_view(container, [None, '-s', '/src/folder', '/dst/folder'])
+
+    assert caught_io.getvalue() == (
+        f'[{"=" * 15}{"-" * 45}] 25% .../src/path/1.jpg\r'
+        f'[{"=" * 30}{"-" * 30}] 50% .../src/path/2.jpg\r'
+        f'[{"=" * 45}{"-" * 15}] 75% .../src/path/3.jpg\r'
+        f'[{"=" * 60}{"-" * 0 }] 100% .../src/path/4.jpg\r'
+    )
