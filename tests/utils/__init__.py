c99ee873e60eb3836f551484cad2da6f17361cd0
rezolvent <rezx10m@gmail.com>
2021-04-25 08:49:34 +0300

    Delete the no longer needed "assert_dict_equal" util

--- tests/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .comparers import assert_dict_equal
--- tests/utils/comparers.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-
-
-def assert_dict_equal(*args, **kwargs):
-    test_case = TestCase()
-    test_case.maxDiff = None
-    test_case.assertDictEqual(*args, **kwargs)
