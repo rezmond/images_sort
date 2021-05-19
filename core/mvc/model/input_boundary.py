b9282390f29c842df207d26c5068fbfe3000909b
rezolvent <rezx10m@gmail.com>
2021-05-19 17:52:52 +0300

    Update the InputBoundary base class

--- core/mvc/model/input_boundary.py
+++ core/mvc/model/input_boundary.py
@@ -1,5 +1,31 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from typeguard import typechecked
 
 
 class InputBoundary(ABC):
-    pass
+
+    @typechecked
+    @abstractmethod
+    def clean_mode(self, enable: bool) -> None:
+        '''Set clean mode'''
+
+    @typechecked
+    @abstractmethod
+    def scan_mode(self, enable: bool) -> None:
+        '''Set scan mode'''
+
+    @typechecked
+    @abstractmethod
+    def move_mode(self, enable: bool) -> None:
+        '''Set move mode'''
+
+    @typechecked
+    @abstractmethod
+    def set_dst_folder(self, value: str) -> None:
+        ''''''
+
+    @typechecked
+    @abstractmethod
+    def set_src_folder(self, value: str) -> None:
+        ''''''
