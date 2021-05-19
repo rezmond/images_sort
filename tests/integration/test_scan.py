c2139a5d3c1198b707dd2a7d41f557f67e1c2c5d
rezolvent <rezx10m@gmail.com>
2021-05-19 17:52:16 +0300

    Implement the test case for the move begining

--- tests/integration/test_scan.py
+++ tests/integration/test_scan.py
@@ -116,3 +116,22 @@ def test_cancel_move_log(container):
 
     assert sys_exit_mock.type == SystemExit
     assert sys_exit_mock.value.code == 0
+
+
+def test_creates_target_folder(container):
+    caught_io = io.StringIO()
+    input_io = io.StringIO('n\n')
+    argv_args = [None, '-m', '/src/folder', '/dst/folder']
+    exif_data_getter_mock = Mock(return_value='2000-01-01T12:00:00')
+    with container.exif_data_getter.override(exif_data_getter_mock), \
+        contextlib.redirect_stdout(caught_io), \
+        redirect_stdin(input_io), \
+            with_presenter(container, argv_args) as presenter:
+        presenter.show()
+
+    expected_value = base_scan_output + \
+        ('The "/dst/folder" folder does not exist.\n'
+            'Do You want to create it [y/N]: '
+         )
+
+    assert_lines_equal(caught_io.getvalue(), expected_value)
