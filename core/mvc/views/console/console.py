bbb31d1ba7aa7be36eb5b850e64685a793b0fb6c
rezolvent <rezx10m@gmail.com>
2021-06-14 09:08:58 +0300

    Break down the view-console module into the separate files

--- core/mvc/views/console/console.py
+++ core/mvc/views/console/console.py
@@ -1,5 +1,4 @@
 import sys
-import argparse
 from typing import Callable, ContextManager, Iterable
 
 import click
@@ -8,42 +7,8 @@ from typeguard import typechecked
 from utils import report_line, report_devider
 from core.types import MoveReport, MoveResult, ScanReport
 from libs import Either, Left, Right
-from ..controllers import IoInteractor, ControllerBase
-
-MAIN_PROGRAMM = 'sorter.py'
-
-parser = argparse.ArgumentParser(
-    description='Gropus some images by EXIF data',
-    prog=MAIN_PROGRAMM,
-)
-parser.add_argument(
-    'src',
-    type=str,
-    help='the source folder full path.')
-parser.add_argument(
-    'dst',
-    type=str,
-    help='the destination folder full path.')
-parser.add_argument(
-    '-v', '--verbosity',
-    default=0,
-    type=int,
-    help='verbosity level')
-parser.add_argument(
-    '-s', '--scan',
-    default=False,
-    action='store_true',
-    help='start the scan process')
-parser.add_argument(
-    '-m', '--move',
-    default=False,
-    action='store_true',
-    help='start moving directly after scan process')
-parser.add_argument(
-    '-c', '--clean',
-    default=False,
-    action='store_true',
-    help='remove the duplicates and actually move the files')
+from ...controllers import IoInteractor, ControllerBase
+from .parser import parser
 
 
 class ConsoleView(IoInteractor):
---
+++ core/mvc/views/console/parser.py
@@ -0,0 +1,37 @@
+import argparse
+
+MAIN_PROGRAMM = 'sorter.py'
+
+parser = argparse.ArgumentParser(
+    description='Gropus some images by EXIF data',
+    prog=MAIN_PROGRAMM,
+)
+
+parser.add_argument(
+    'src',
+    type=str,
+    help='the source folder full path.')
+parser.add_argument(
+    'dst',
+    type=str,
+    help='the destination folder full path.')
+parser.add_argument(
+    '-v', '--verbosity',
+    default=0,
+    type=int,
+    help='verbosity level')
+parser.add_argument(
+    '-s', '--scan',
+    default=False,
+    action='store_true',
+    help='start the scan process')
+parser.add_argument(
+    '-m', '--move',
+    default=False,
+    action='store_true',
+    help='start moving directly after scan process')
+parser.add_argument(
+    '-c', '--clean',
+    default=False,
+    action='store_true',
+    help='remove the duplicates and actually move the files')
