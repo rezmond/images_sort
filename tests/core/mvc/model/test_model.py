1e9a9a2371c9818bc3736885489b708e20f8c592
rezolvent <rezx10m@gmail.com>
2021-06-27 13:41:20 +0300

    Fix the "test_move_call" test

--- tests/core/mvc/model/test_model.py
+++ tests/core/mvc/model/test_model.py
@@ -3,10 +3,12 @@ from unittest.mock import call, Mock, MagicMock, PropertyMock
 
 import pytest
 
+from core.types import FileWay, MoveType, MoveReport, MoveResult
 from core.entities.fs import FsManipulatorBase
 from core.entities.scanner.base import ScannerBase
 from core.entities.mover import MoverBase
 from core.utils.base import Observable
+from core.mvc.model import OutputBoundary
 
 
 @pytest.fixture
@@ -32,22 +34,48 @@ def subscription_context(container):
 
 
 def test_move_call(container):
+    move_types = [FileWay(src=str(x), type=MoveType.MEDIA) for x in range(3)]
+
     scanner_mock = Mock(spec=ScannerBase, **{
-        'scan.return_value': (x for x in range(3))
+        'scan.return_value': move_types
     })
-    mover_mock = Mock(spec=MoverBase)
+
+    def map_mock(move):
+        move('')
+
+    mover_mock = Mock(spec=MoverBase, **{
+        'set_dst_folder.return_value': Mock(
+            **{'either.return_value': Mock(map=map_mock)}),
+        'move.return_value': Mock(spec=MoveReport, result=MoveResult.MOVED),
+    })
     with container.fs_manipulator.override(Mock(spec=FsManipulatorBase)),\
             container.observable.override(MagicMock(spec=Observable)),\
             container.scanner.override(scanner_mock),\
             container.mover.override(mover_mock):
         model = container.model()
 
-    model.set_dst_folder('dst')
+    def on_move_started(move_gen, length):
+        list(move_gen)
+
+    output_boundary_mock = Mock(
+        spec=OutputBoundary, on_move_started=on_move_started)
+
+    model.set_output_boundary(output_boundary_mock)
+
     model.set_src_folder('src')
-    model.move()
+    model.scan()
 
     scanner_mock.scan.assert_called_once_with('src')
-    mover_mock.move.assert_has_calls([call(x, 'dst', False) for x in range(3)])
+
+    model.set_dst_folder('dst')
+    model.move()
+
+    mover_mock.set_dst_folder.assert_called_once_with('dst')
+
+    no_clean_mode = False
+    mover_mock.move.assert_has_calls([
+        call(x, no_clean_mode) for x in move_types
+    ])
 
 
 def test_on_move_finish_subscribe(subscription_context):
