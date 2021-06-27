2f7a49d9a37162702ac8647ca7c50e37414cbaa4
rezolvent <rezx10m@gmail.com>
2021-06-27 14:45:05 +0300

    Delete no longer needed "test_clean_mode_safe" test case

--- tests/core/mvc/model/test_model.py
+++ tests/core/mvc/model/test_model.py
@@ -14,15 +14,30 @@ def get_model(container, **mocks):
     return model
 
 
-def test_move_call(container):
-
-    move_types = [FileWay(src=str(x), type=MoveType.MEDIA) for x in range(3)]
-
-    def map_mock(move):
-        move('')
-
+def scan(model, src):
     def on_move_started(move_gen, length):
         list(move_gen)
+
+    output_boundary_mock = Mock(
+        spec=OutputBoundary, on_move_started=on_move_started)
+
+    model.set_output_boundary(output_boundary_mock)
+
+    model.set_src_folder(src)
+    model.scan()
+
+
+def move(model, dst):
+    model.set_dst_folder(dst)
+    model.move()
+
+
+move_types = [FileWay(src=str(x), type=MoveType.MEDIA) for x in range(3)]
+
+
+def test_move_call(container):
+    def map_mock(move):
+        move('')
 
     scanner_mock = Mock(spec=ScannerBase, **{
         'scan.return_value': move_types
@@ -40,38 +55,20 @@ def test_move_call(container):
             call(x, is_clean_mode) for x in move_types
         ])
 
-    output_boundary_mock = Mock(
-        spec=OutputBoundary, on_move_started=on_move_started)
-
     model = get_model(
         container,
         scanner=scanner_mock,
         mover=mover_mock,
     )
 
-    model.set_output_boundary(output_boundary_mock)
-
-    model.set_src_folder('src')
-    model.scan()
+    scan(model, 'src')
 
     scanner_mock.scan.assert_called_once_with('src')
 
-    model.set_dst_folder('dst')
-    model.move()
+    move(model, 'dst')
 
     mover_mock.set_dst_folder.assert_called_once_with('dst')
     assert_moved()
-
-
-def test_clean_mode_safe(container):
-    scanner_mock = Mock(spec=ScannerBase, **{'scan.return_value': [None]})
-    mover_mock = Mock(spec=MoverBase)
-    with container.mover.override(mover_mock),\
-            container.scanner.override(scanner_mock):
-        model = container.model()
-    model.move()
-
-    mover_mock.move.assert_called_once_with(None, None, False)
 
 
 def test_clean_mode_dangerously(container):
@@ -80,6 +77,7 @@ def test_clean_mode_dangerously(container):
     with container.mover.override(mover_mock),\
             container.scanner.override(scanner_mock):
         model = container.model()
+
     model.clean_mode(True)
     model.move()
 
