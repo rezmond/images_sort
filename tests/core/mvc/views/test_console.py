d8ac96537edc2fe2865cc89950b138abb0b6c385
rezolvent <rezx10m@gmail.com>
2021-07-15 11:41:08 +0300

    Implement the "test_required_src_folder" test case

--- tests/core/mvc/views/test_console.py
+++ tests/core/mvc/views/test_console.py
@@ -4,6 +4,7 @@ from unittest.mock import patch, call, Mock, MagicMock
 
 import pytest
 
+from core.entities.exceptions import NoArgumentPassedError
 from core.mvc.controllers import ControllerBase
 from core.mvc.model import MoverModel
 from core.types import MoveReport, FileWay, MoveType, MoveResult
@@ -27,6 +28,23 @@ def view(container, controller_mock):
     yield view
 
 
+def assert_argument_errors_contains(
+        exc_info, std_err_file, *msg_containing_words):
+    assert exc_info.value.code == 2
+
+    printed_error = std_err_file.getvalue()
+    for words in msg_containing_words:
+        assert words in printed_error
+
+
+@contextlib.contextmanager
+def raises_argument_errors():
+    std_err_file = io.StringIO()
+    with contextlib.redirect_stderr(std_err_file), \
+            pytest.raises(SystemExit) as exc_info:
+        yield (exc_info, std_err_file)
+
+
 def test_filled_params(view, controller_mock):
 
     with patch('sys.argv', [None, '/src/folder', '/dst/folder']):
@@ -36,6 +54,14 @@ def test_filled_params(view, controller_mock):
         call.set_src_folder('/src/folder'),
         call.set_dst_folder('/dst/folder'),
     ])
+
+
+def test_required_src_folder(view):
+    with patch('sys.argv', [None]),\
+            raises_argument_errors() as exc_data:
+        view.show()
+
+    assert_argument_errors_contains(*exc_data, 'src', 'required')
 
 
 def test_help_param(view, controller_mock, model_mock):
