6b276334ac919e451be253faab91c733e9722dac
rezolvent <rezx10m@gmail.com>
2021-07-15 15:44:21 +0300

    Partially implement the code fore real execution

--- start.py
+++ start.py
@@ -6,7 +6,11 @@ from containers import Container
 def main():
     container = Container()
     container.wire(modules=[sys.modules[__name__]])
+
     controller = container.controller()
+    view = container.view()
+
+    controller.set_io_interactor(view)
     controller.show()
 
 
