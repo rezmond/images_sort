5a7ae3fb20c1ad997d18b0fb80eeb6c06248133e
rezolvent <rezx10m@gmail.com>
2021-07-16 11:39:04 +0300

    Implement the "test_moving_complex_data" test case

--- tests/core/mvc/model/test_model.py
+++ tests/core/mvc/model/test_model.py
@@ -14,7 +14,7 @@ scanner_mock = None
 
 @pytest.fixture
 def get_model(container):
-    def model_supplier(**mocks):
+    def model_supplier(move_types, **mocks):
         global mover_mock
         global scanner_mock
 
@@ -65,28 +65,35 @@ def move(model, dst):
     model.move()
 
 
-def assert_moved(dst, is_clean_mode):
+def assert_moved(dst, move_types, is_clean_mode):
     mover_mock.set_dst_folder.assert_called_once_with(dst)
+    assert mover_mock.move.call_count == len(move_types)
     mover_mock.move.assert_has_calls([
         call(x, is_clean_mode) for x in move_types
     ])
 
 
-move_types = [FileWay(src=str(x), type=MoveType.MEDIA) for x in range(3)]
+simple_move_types = [FileWay(src=str(x), type=MoveType.MEDIA)
+                     for x in range(3)]
+complex_move_types = [
+    FileWay(src='/src/path/1', type=MoveType.MEDIA),
+    FileWay(src='/src/path/2', type=MoveType.NO_DATA),
+    FileWay(src='/src/path/3', type=MoveType.NO_MEDIA),
+]
 
 
 def test_safe_move(get_model):
-    model = get_model()
+    model = get_model(simple_move_types)
 
     scan(model, 'src')
     scanner_mock.scan.assert_called_once_with('src')
 
     move(model, 'dst')
-    assert_moved('dst', False)
+    assert_moved('dst', simple_move_types, False)
 
 
 def test_danger_move(get_model):
-    model = get_model()
+    model = get_model(simple_move_types)
 
     scan(model, 'src')
     scanner_mock.scan.assert_called_once_with('src')
@@ -94,4 +101,14 @@ def test_danger_move(get_model):
     model.clean_mode(True)
 
     move(model, 'dst')
-    assert_moved('dst', True)
+    assert_moved('dst', simple_move_types, True)
+
+
+def test_moving_complex_data(get_model):
+    model = get_model(complex_move_types)
+
+    scan(model, 'src')
+    scanner_mock.scan.assert_called_once_with('src')
+
+    move(model, 'dst')
+    assert_moved('dst', (complex_move_types[0], ), False)
